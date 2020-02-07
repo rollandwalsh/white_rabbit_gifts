be65dabd25a35d1e525686fa25c146b3e7e254c1
Rolland Walsh <rollandwalsh@gmail.com>
2020-02-07 09:43:33 -0800

    Add Google Analytics code

--- header.php
+++ header.php
@@ -34,6 +34,16 @@
 		?>
 		
 		<link rel="stylesheet" href="https://use.typekit.net/nfp5qii.css">
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157957491-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			
+			gtag('config', 'UA-157957491-1');
+		</script>
 	</head>
 
 	<body 

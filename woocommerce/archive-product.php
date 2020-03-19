9211beb9b0e4d8a8f119ad2d63ce6cb7148ba2cb
Rolland Walsh <rollandwalsh@gmail.com>
2020-03-19 12:14:03 -0700

    Wraps WC pages in <article>s

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -31,82 +31,83 @@ do_action( 'woocommerce_before_main_content' );
 ?>
 	<div id="primary">
 		<section id="content" role="main" class="site-content">
-
-			<header class="woocommerce-products-header">
-				<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-					<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-				<?php endif; ?>
-			
+			<article class="post">
+				<header class="woocommerce-products-header">
+					<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+						<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+					<?php endif; ?>
+				
+					<?php
+					/**
+					 * Hook: woocommerce_archive_description.
+					 *
+					 * @hooked woocommerce_taxonomy_archive_description - 10
+					 * @hooked woocommerce_product_archive_description - 10
+					 */
+					do_action( 'woocommerce_archive_description' );
+					?>
+				</header>
+				
+				<form role="search" method="get" id="searchform" class="searchform" action="https://whiterabbit.gifts/">
+					<div>
+						<label class="screen-reader-text" for="s">Search for:</label>
+						<input type="text" value="" name="s" id="s">
+						<input type="submit" id="searchsubmit" value="Search">
+					</div>
+				</form>
+				
 				<?php
-				/**
-				 * Hook: woocommerce_archive_description.
-				 *
-				 * @hooked woocommerce_taxonomy_archive_description - 10
-				 * @hooked woocommerce_product_archive_description - 10
-				 */
-				do_action( 'woocommerce_archive_description' );
-				?>
-			</header>
-			
-			<form role="search" method="get" id="searchform" class="searchform" action="https://whiterabbit.gifts/">
-				<div>
-					<label class="screen-reader-text" for="s">Search for:</label>
-					<input type="text" value="" name="s" id="s">
-					<input type="submit" id="searchsubmit" value="Search">
-				</div>
-			</form>
-			
-			<?php
-			if ( woocommerce_product_loop() ) {
-			
-				/**
-				 * Hook: woocommerce_before_shop_loop.
-				 *
-				 * @hooked woocommerce_output_all_notices - 10
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-			
-				woocommerce_product_loop_start();
-			
-				if ( wc_get_loop_prop( 'total' ) ) {
-					while ( have_posts() ) {
-						the_post();
-			
-						/**
-						 * Hook: woocommerce_shop_loop.
-						 */
-						do_action( 'woocommerce_shop_loop' );
-			
-						wc_get_template_part( 'content', 'product' );
+				if ( woocommerce_product_loop() ) {
+				
+					/**
+					 * Hook: woocommerce_before_shop_loop.
+					 *
+					 * @hooked woocommerce_output_all_notices - 10
+					 * @hooked woocommerce_result_count - 20
+					 * @hooked woocommerce_catalog_ordering - 30
+					 */
+					do_action( 'woocommerce_before_shop_loop' );
+				
+					woocommerce_product_loop_start();
+				
+					if ( wc_get_loop_prop( 'total' ) ) {
+						while ( have_posts() ) {
+							the_post();
+				
+							/**
+							 * Hook: woocommerce_shop_loop.
+							 */
+							do_action( 'woocommerce_shop_loop' );
+				
+							wc_get_template_part( 'content', 'product' );
+						}
 					}
+				
+					woocommerce_product_loop_end();
+				
+					/**
+					 * Hook: woocommerce_after_shop_loop.
+					 *
+					 * @hooked woocommerce_pagination - 10
+					 */
+					do_action( 'woocommerce_after_shop_loop' );
+				} else {
+					/**
+					 * Hook: woocommerce_no_products_found.
+					 *
+					 * @hooked wc_no_products_found - 10
+					 */
+					do_action( 'woocommerce_no_products_found' );
 				}
-			
-				woocommerce_product_loop_end();
-			
+				
 				/**
-				 * Hook: woocommerce_after_shop_loop.
+				 * Hook: woocommerce_after_main_content.
 				 *
-				 * @hooked woocommerce_pagination - 10
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 				 */
-				do_action( 'woocommerce_after_shop_loop' );
-			} else {
-				/**
-				 * Hook: woocommerce_no_products_found.
-				 *
-				 * @hooked wc_no_products_found - 10
-				 */
-				do_action( 'woocommerce_no_products_found' );
-			}
-			
-			/**
-			 * Hook: woocommerce_after_main_content.
-			 *
-			 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-			 */
-			do_action( 'woocommerce_after_main_content' );
-			?>
+				do_action( 'woocommerce_after_main_content' );
+				?>
+			</article>
 		</section>
 	</div>
 

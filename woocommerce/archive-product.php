822e9210af940f7a911f667cb23e37654f54bbe8
Rolland Walsh <rollandwalsh@gmail.com>
2020-03-19 12:29:21 -0700

    WC upgrade

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -17,18 +17,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
-get_header( 'shop' );
+get_header( 'shop' ); ?>
 
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action( 'woocommerce_before_main_content' );
-
-?>
 	<div id="primary">
 		<section id="content" role="main" class="site-content">
 			<article class="post">
@@ -48,6 +38,17 @@ do_action( 'woocommerce_before_main_content' );
 					?>
 				</header>
 				
+				<?php
+				/**
+				 * Hook: woocommerce_before_main_content.
+				 *
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 * @hooked WC_Structured_Data::generate_website_data() - 30
+				 */
+				do_action( 'woocommerce_before_main_content' );	
+				?>
+				
 				<form role="search" method="get" id="searchform" class="searchform" action="https://whiterabbit.gifts/">
 					<div>
 						<label class="screen-reader-text" for="s">Search for:</label>
@@ -56,57 +57,59 @@ do_action( 'woocommerce_before_main_content' );
 					</div>
 				</form>
 				
-				<?php
-				if ( woocommerce_product_loop() ) {
-				
-					/**
-					 * Hook: woocommerce_before_shop_loop.
-					 *
-					 * @hooked woocommerce_output_all_notices - 10
-					 * @hooked woocommerce_result_count - 20
-					 * @hooked woocommerce_catalog_ordering - 30
-					 */
-					do_action( 'woocommerce_before_shop_loop' );
-				
-					woocommerce_product_loop_start();
-				
-					if ( wc_get_loop_prop( 'total' ) ) {
-						while ( have_posts() ) {
-							the_post();
-				
-							/**
-							 * Hook: woocommerce_shop_loop.
-							 */
-							do_action( 'woocommerce_shop_loop' );
-				
-							wc_get_template_part( 'content', 'product' );
+				<div class="post-content">
+					<?php
+					if ( woocommerce_product_loop() ) {
+					
+						/**
+						 * Hook: woocommerce_before_shop_loop.
+						 *
+						 * @hooked woocommerce_output_all_notices - 10
+						 * @hooked woocommerce_result_count - 20
+						 * @hooked woocommerce_catalog_ordering - 30
+						 */
+						do_action( 'woocommerce_before_shop_loop' );
+					
+						woocommerce_product_loop_start();
+					
+						if ( wc_get_loop_prop( 'total' ) ) {
+							while ( have_posts() ) {
+								the_post();
+					
+								/**
+								 * Hook: woocommerce_shop_loop.
+								 */
+								do_action( 'woocommerce_shop_loop' );
+					
+								wc_get_template_part( 'content', 'product' );
+							}
 						}
+					
+						woocommerce_product_loop_end();
+					
+						/**
+						 * Hook: woocommerce_after_shop_loop.
+						 *
+						 * @hooked woocommerce_pagination - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop' );
+					} else {
+						/**
+						 * Hook: woocommerce_no_products_found.
+						 *
+						 * @hooked wc_no_products_found - 10
+						 */
+						do_action( 'woocommerce_no_products_found' );
 					}
-				
-					woocommerce_product_loop_end();
-				
+					
 					/**
-					 * Hook: woocommerce_after_shop_loop.
+					 * Hook: woocommerce_after_main_content.
 					 *
-					 * @hooked woocommerce_pagination - 10
+					 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 					 */
-					do_action( 'woocommerce_after_shop_loop' );
-				} else {
-					/**
-					 * Hook: woocommerce_no_products_found.
-					 *
-					 * @hooked wc_no_products_found - 10
-					 */
-					do_action( 'woocommerce_no_products_found' );
-				}
-				
-				/**
-				 * Hook: woocommerce_after_main_content.
-				 *
-				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-				 */
-				do_action( 'woocommerce_after_main_content' );
-				?>
+					do_action( 'woocommerce_after_main_content' );
+					?>
+				</div>
 			</article>
 		</section>
 	</div>

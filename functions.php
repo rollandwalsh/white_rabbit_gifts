744705b375d4977f0bafce811ac4633a1377b1d5
Rolland Walsh <rollandwalsh@gmail.com>
2020-03-19 11:49:48 -0700

    Further WooCommerce support

--- functions.php
+++ functions.php
@@ -41,6 +41,14 @@ function custom_single_template($single) {
 		endforeach;
 }
 
+/*-----------------------------------------------------------------------------------*/
+/* Register WooCommerce Support
+/*-----------------------------------------------------------------------------------*/
+
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 /*-----------------------------------------------------------------------------------*/
 /* Register main menu for Wordpress use
